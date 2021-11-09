b8e64b2c3fb92045ad64751a6d20d2403caee3c8
saityan <naminatosky@gmail.com>
2021-11-09 18:26:34 +0300

    ViewHolderBased.kt added

--- app/src/main/java/ru/geekbrains/nasawannabeapp/view/viewmodel/RecyclerActivityAdapter.kt
+++ app/src/main/java/ru/geekbrains/nasawannabeapp/view/viewmodel/RecyclerActivityAdapter.kt
@@ -11,14 +11,14 @@ import ru.geekbrains.nasawannabeapp.databinding.ActivityRecyclerItemMarsBinding
 class RecyclerActivityAdapter (
     private var clickListener: RecyclerClickListener,
     private var data: List<RecyclerData>
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+) : RecyclerView.Adapter<ViewHolderBased>() {
 
     override fun getItemViewType(position: Int): Int {
         if (position == 0) return TYPE_HEADER
-        return if(this.data[position].planetDescription.isNullOrBlank()) TYPE_EARTH else TYPE_MARS
+        return if(this.data[position].planetDescription.isBlank()) TYPE_EARTH else TYPE_MARS
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderBased {
         return when (viewType) {
             TYPE_EARTH -> {
                 val binding: ActivityRecyclerItemEarthBinding =
@@ -41,26 +41,16 @@ class RecyclerActivityAdapter (
         }
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when (getItemViewType(position)) {
-            TYPE_EARTH -> {
-                (holder as EarthViewHolder).bind(data[position])
-            }
-            TYPE_MARS -> {
-                (holder as MarsViewHolder).bind(data[position])
-            }
-            TYPE_HEADER -> {
-                (holder as HeaderViewHolder).bind(data[position])
-            }
-        }
+    override fun onBindViewHolder(holder: ViewHolderBased, position: Int) {
+            holder.bind(data[position])
     }
 
     override fun getItemCount(): Int {
         return data.size
     }
 
-    inner class EarthViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        fun bind(data: RecyclerData) {
+    inner class EarthViewHolder(view: View) : ViewHolderBased(view) {
+        override fun bind(data: RecyclerData) {
             ActivityRecyclerItemEarthBinding.bind(itemView).apply {
                 descriptionTextView.text = data.planetDescription
                 wikiImageView.setOnClickListener {
@@ -70,8 +60,8 @@ class RecyclerActivityAdapter (
         }
     }
 
-    inner class MarsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        fun bind(data: RecyclerData) {
+    inner class MarsViewHolder(view: View) : ViewHolderBased(view) {
+        override fun bind(data: RecyclerData) {
             ActivityRecyclerItemMarsBinding.bind(itemView).apply {
                 marsImageView.setOnClickListener {
                     clickListener.onItemClick(data)
@@ -80,8 +70,8 @@ class RecyclerActivityAdapter (
         }
     }
 
-    inner class HeaderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        fun bind(data: RecyclerData) {
+    inner class HeaderViewHolder(view: View) : ViewHolderBased(view) {
+        override fun bind(data: RecyclerData) {
             ActivityRecyclerItemHeaderBinding.bind(itemView).apply {
                 root.setOnClickListener {
                     clickListener.onItemClick(data)
---
+++ app/src/main/java/ru/geekbrains/nasawannabeapp/view/viewmodel/ViewHolderBased.kt
@@ -0,0 +1,8 @@
+package ru.geekbrains.nasawannabeapp.view.viewmodel
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class ViewHolderBased(view: View): RecyclerView.ViewHolder(view) {
+    abstract fun bind (data: RecyclerData)
+}
